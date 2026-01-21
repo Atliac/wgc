7b26b3fb0552142d9514b854d419076527d906d7
Atliac <Atliac@outlook.com>
2026-01-21 20:20:13 +0800

    Set up initial library structure, dependencies, and CI workflow for Windows Graphics Capture.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! A simple wrapper for Windows Graphics Capture
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod settings;
+pub mod wgc;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use settings::*;
+pub use wgc::*;
